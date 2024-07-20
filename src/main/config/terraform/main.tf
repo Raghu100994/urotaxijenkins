eb0bed46568eff96cdde1e65472d916837cd6864
Raghu100994 <r.raghunathan1994@gmail.com>
2024-07-20 20:41:36 +0530

    added tf code

---
+++ src/main/config/terraform/main.tf
@@ -0,0 +1,150 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_vpc" "urotaxivpc" {
+  cidr_block = var.urotaxivpc_cidr
+  tags = {
+    Name = "urotaxivpc"
+  }
+}
+
+resource "aws_subnet" "urotaxi_pubsn1" {
+  vpc_id = aws_vpc.urotaxivpc.id
+  cidr_block = var.urotaxi_pubsn1_cidr
+  tags = {
+    Name = "urotaxiPusn1"
+  }
+  availability_zone = "ap-south-1a"
+}
+
+resource "aws_subnet" "urotaxi_prvsn2" {
+  vpc_id = aws_vpc.urotaxivpc.id
+  cidr_block = aws_subnet.urotaxi_prvsn2_cidr
+  tags = {
+    Name = "urotaxiPrvsn2"
+  }
+  availability_zone = "ap-south-1b"
+}
+
+resource "aws_subnet" "urotaxi_prvsn3" {
+  vpc_id = aws_vpc.urotaxivpc.id
+  cidr_block = aws_subnet.urotaxi_prvsn3_cidr
+  tags = {
+    Name = "urotaxiPrvsn3"
+  }
+  availability_zone = "ap-south-1c"
+}
+
+resource "aws_internet_gateway" "urotaxiig" {
+  vpc_id = aws_vpc.urotaxivpc.id
+  tags = {
+    Name = "urotaxiig" 
+  }
+}
+
+resource "aws_route_table" "urotaxiigrt" {
+  vpc_id = aws_vpc.urotaxivpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.urotaxiig.id
+  }
+  tags = {
+    Name = "uriotaxiigrt"
+  }
+}
+
+resource "aws_route_table_association" "uriotaxiigrtassociation" {
+  route_table_id = aws_route_table.urotaxiigrt.id
+  subnet_id = aws_subnet.urotaxi_pubsn1
+}
+
+resource "aws_security_group" "urotaxijavaserversg" {
+  vpc_id = aws_vpc.urotaxivpc.id
+  ingress {
+    from_port = "8080"
+    to_port = "8080"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = "22"
+    to_port = "22"
+    protocol = "ssh"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "urotaxiJserversg"
+  }
+}
+
+resource "aws_security_group" "urotaxidbsg" {
+  vpc_id = aws_vpc.urotaxivpc.id
+  ingress {
+    from_port = "3306"
+    to_port = "3306"
+    protocol = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "urotaxidbsg"
+  }
+}
+
+resource "aws_db_subnet_group" "urotaxidbsngrp" {
+  name = "urotaxidbsubnetgrp"
+  subnet_ids = [aws_subnet.urotaxi_prvsn2, aws_subnet.urotaxi_prvsn3]
+  tags = {
+    Name = "urotaxidbsngroup"
+  }
+}
+
+resource "aws_db_instance" "urotaxidb" {
+  allocated_storage    = 10
+  db_name              = var.db_name
+  engine               = "mysql"
+  engine_version       = var.db_engine_version
+  instance_class       = var.db_instance_type
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+  db_subnet_group_name = aws_db_subnet_group.urotaxidbsngrp.name
+}
+
+resource "aws_key_pair" "urotaxikp" {
+  key_name = "urotaxikey"
+  public_key = var.urotaxi_public_key
+}
+
+resource "aws_instance" "urotaxiec2" {
+  vpc_security_group_ids = [aws_security_group.urotaxijavaserversg.id]
+  subnet_id = aws_subnet.urotaxi_pubsn1.id
+  ami = var.ami_id
+  key_name = aws_key_pair.urotaxikp
+  instance_type = var.instance_shape
+  associate_public_ip_address = true
+  tags = {
+    Name = "urotaxiserver"
+  }
+}
---
+++ src/main/config/terraform/variables.tf
@@ -0,0 +1,36 @@
+variable "urotaxivpc_cidr" {
+  type = string
+}
+variable "urotaxi_pubsn1_cidr" {
+  type = string
+}
+variable "urotaxi_prvsn2_cidr" {
+  type = string
+}
+variable "urotaxi_prvsn3_cidr" {
+  type = string
+}
+variable "db_instance_type" {
+  type = string
+}
+variable "db_username" {
+  type = string
+}
+variable "db_password" {
+  type = string
+}
+variable "db_name" {
+  type = string
+}
+variable "db_engine_version" {
+  type = string
+}
+variable "ami_id" {
+  type = string
+}
+variable "instance_shape" {
+  type = string
+}
+variable "urotaxi_public_key" {
+  type = string
+}
